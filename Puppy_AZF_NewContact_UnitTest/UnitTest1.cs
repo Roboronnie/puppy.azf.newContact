e04c406ca2284de73bcdc5573e221bfefb9c97c6
Roboronnie <leex4920@gmail.com>
2020-04-14 23:22:46 -0500

    add unit test proj

---
+++ Puppy_AZF_NewContact_UnitTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Puppy_AZF_NewContact_UnitTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
